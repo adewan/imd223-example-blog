0710b81a6c4c2040c8a0d58dd67523c5b1f76a1a
Faculty <faculty@WYFY.local>
2012-05-14 21:34:04 -0400

    Fixes ability file comments

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,9 +2,8 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
-    # Define abilities for the passed in user here. For example:
-    #
-    user ||= User.new # guest user (not logged in)
+    
+    user ||= User.new
 
     if user.admin?
       can :manage, :all
